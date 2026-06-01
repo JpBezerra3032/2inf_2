aeabb2dc0ac6a829a3daeb6d728f98cf816490d5
João <joaopedrobezer12@gmail.com>
2026-06-01 11:10:38 -0300

    post_aluno

--- src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
+++ src/main/java/com/eeep/ocorrencia/controller/AlunoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Controller
 public class AlunoController {
-    @GetMapping("/")
+    @GetMapping("/aluno")
     public String abrirFormulario(Model model) {
         model.addAttribute("aluno", new Aluno());
         return "cadastro";
@@ -24,6 +24,8 @@ public class AlunoController {
         System.out.println(aluno.getNumero());
         System.out.println(aluno.getNome());
         System.out.println(aluno.getSexo());
+        System.out.println(aluno.getTurma());
+        System.out.println(aluno.getMatricula());
         return "cadastro";
     }
 
